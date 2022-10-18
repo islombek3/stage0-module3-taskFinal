f3ff9277bf63cd6f475d1b2f320628dab061d652
Islombek Akhmadjonov <81472194+islombek3@users.noreply.github.com>
2022-10-18 07:21:32 -0700

    add logic

--- src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
@@ -2,6 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumbersPrinter {
     public void printIsPositive(int number) {
-
+        String result = (number > 0)? "true": "false";
+        System.out.println(result);
     }
 }
