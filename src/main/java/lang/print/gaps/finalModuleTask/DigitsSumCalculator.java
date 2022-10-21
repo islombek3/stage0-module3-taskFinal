b358ad283d1e95d2a8bb5bbfad9338b545f21ff1
Islombek Akhmadjonov <81472194+islombek3@users.noreply.github.com>
2022-10-21 04:37:07 -0700

    add logic

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,12 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int rem, sum = 0;
+        while(number > 0){
+            rem = number % 10;
+            sum = sum + rem;
+            number = number / 10;
+        }
+        System.out.println(sum);
     }
 }
