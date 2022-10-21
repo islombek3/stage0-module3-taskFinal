0b615ce52ae3cd25c023abcc13f6dc883347f248
Islombek Akhmadjonov <81472194+islombek3@users.noreply.github.com>
2022-10-21 05:30:23 -0700

    fix val type

--- src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
@@ -2,7 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumbersPrinter {
     public void printIsPositive(int number) {
-        String result = (number > 0)? "true": "false";
+        Boolean result = (number > 0)? "true": "false";
         System.out.println(result);
     }
 }
