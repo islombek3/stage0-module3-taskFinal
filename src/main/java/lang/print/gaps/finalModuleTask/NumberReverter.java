74a44633e7fba97f4421f05c7692bfd79f104e7c
Islombek Akhmadjonov <81472194+islombek3@users.noreply.github.com>
2022-10-21 04:36:01 -0700

    add logic

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,12 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int rem, rev = 0;
+        while(number > 0){
+            rem = number % 10;
+            rev = rev * 10 + rem;
+            number = number / 10;
+        }
+        System.out.println(rev)
     }
 }
