8c246a6512413f756956e2c5a977a8662892ff70
Islombek Akhmadjonov <81472194+islombek3@users.noreply.github.com>
2022-10-21 05:16:47 -0700

    change var type

--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,8 +2,8 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
-        int fahrenheit;
-        fahrenheit = (temperatureCelsius * (int)1.8) + 32;
+        double fahrenheit;
+        fahrenheit = (temperatureCelsius * 1.8) + 32;
         System.out.println(fahrenheit);
     }
 }
